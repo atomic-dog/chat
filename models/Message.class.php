22db19742b9964df233abb384f956c990e0437c5
VeroStras <nrvero@gmail.com>
2016-03-07 17:33:10 +0100

    update fichier message

--- models/Message.class.php
+++ models/Message.class.php
@@ -12,76 +12,45 @@ class Message
 	// Déclarer les méthodes
 	// Liste des getters
 	// getter de $id -> getId
-	public function getId_message()
+	public function getIdMessage()
 	{
 		return $this->id_message;// On récupère la propriété id de $this
 		// Pas de $ après ->
 	}
-	public function getContent_message()
+	public function getContentMessage()
 	{
 		return $this->content_message;
 	}
-	public function getDate_message()
+	public function getDateMessage()
 	{
 		return $this->date_message;
 	}
-	public function getId_user_message()
+	public function getIdUserMessage()
 	{
 		return $this->id_user_message;
 	}
 
 	// Liste des setters
-	public function setContent_message($content_message)
+	public function setContentMessage($content_message)
 	{
-		if (strlen($login) > 1 && strlen($login) <1023)
-			$this->login = $login;
+		if (strlen($content_message) > 1 && strlen($content_message) <1023)
+			$this->content_message = $content_message;
 	}
 	
 
 	// Liste des méthodes "autres"
-	// verifier password ?
-	public function verifPassword($password)
-	{
-		return password_verify($password, $this->hash);
-	}
-	// modifier password ?
-	public function editPassword($oldPassword, $newPassword1, $newPassword2)
-	{
-		if ($newPassword1 === $newPassword2)
-		{
-			$newPassword = $newPassword1;
-			if (strlen($newPassword) > 5)
-			{
-				if ($this->verifPassword($oldPassword))
-				{
-					$this->hash = password_hash($newPassword, PASSWORD_BCRYPT, ["cost"=>12]);
-				}
-			}
-		}
-	}
-	public function initPassword($newPassword1, $newPassword2)
-	{
-		if ($this->hash == null)
-		{
-			if ($newPassword1 === $newPassword2)
-			{
-				$newPassword = $newPassword1;
-				if (strlen($newPassword) > 5)
-				{
-					$this->hash = password_hash($newPassword, PASSWORD_BCRYPT, ["cost"=>12]);
-				}
-			}
-		}
-	}
+	
+	
+	
 }
 
 // Tout ça n'a rien a foutre dans le fichier User.class.php, mais c'est plus pratique pour apprendre
 // On va instancier notre classe User
-$user = new User();
+$message = new Message();
 // $user -> objet
 // User -> classe
 // Un objet est une instance d'une classe
-var_dump($user);
+var_dump($message);
 /*
 object(User)[1]
   private 'id' => null
@@ -90,8 +59,8 @@ object(User)[1]
   private 'date' => null
   private 'admin' => null
 */
-$user->setLogin("toto");
-var_dump($user);
+$message->setContentMessage("toto");
+var_dump($message);
 /*
 object(User)[1]
   private 'id' => null
@@ -100,34 +69,5 @@ object(User)[1]
   private 'date' => null
   private 'admin' => null
 */
-$user->setLogin("aa");
-var_dump($user);
-/*
-object(User)[1]
-  private 'id' => null
-  private 'login' => string 'toto' (length=4)
-  private 'hash' => null
-  private 'date' => null
-  private 'admin' => null
-*/
-$user->initPassword("totototo", "totototo");
-var_dump($user);
-/*
-object(User)[1]
-  private 'id' => null
-  private 'login' => string 'toto' (length=4)
-  private 'hash' => string '$2y$12$9n144prWnPaTt2SmtJGj6OVfHX9lZZQVELrQWwQqwD0OHPiYmQzBi' (length=60)
-  private 'date' => null
-  private 'admin' => null
-*/
-$user->initPassword("titititi", "titititi");
-var_dump($user);
-/*
-object(User)[1]
-  private 'id' => null
-  private 'login' => string 'toto' (length=4)
-  private 'hash' => string '$2y$12$9n144prWnPaTt2SmtJGj6OVfHX9lZZQVELrQWwQqwD0OHPiYmQzBi' (length=60)
-  private 'date' => null
-  private 'admin' => null
-*/
-?>
+
+?>git statu
