095504fe6e4c163743d35209be449e3173145b2a
atomic-dog <thegritch@hotmail.fr>
2016-03-08 19:26:55 +0100

    maj header et home

--- apps/header.php
+++ apps/header.php
@@ -1,5 +1,4 @@
 <?php
-/* ##PASCAL ~> Pas besoin de passer par une variable ici, c'est juste une remarque */
 if (isset($_SESSION['id'],$_SESSION['login']))
 {
 	require('views/header_user.phtml');
--- apps/home.php
+++ apps/home.php
@@ -6,12 +6,6 @@
 // var_dump($user->isAdmin());
 // var_dump($user->isAdmin());
 // exit;
-// if ( $_SESSION['role'] == $user->isAdmin() )
-// 	$home = 'home';
-// else if ( $_SESSION['role'] != $user->isAdmin() )
-// 	$home = 'home';
-// else
-// 	$home = 'home_login';
 
 if (isset($_SESSION['id'],$_SESSION['login']))
 {
