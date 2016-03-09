33065df00ead91e666257cfa0b96a27cbac8ef04
atomic-dog <thegritch@hotmail.fr>
2016-03-09 09:45:41 +0100

    maj home

--- apps/home.php
+++ apps/home.php
@@ -6,18 +6,12 @@
 // var_dump($user->isAdmin());
 // var_dump($user->isAdmin());
 // exit;
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
 // if ( $_SESSION['role'] == $user->isAdmin() )
 // 	$home = 'home';
 // else if ( $_SESSION['role'] != $user->isAdmin() )
 // 	$home = 'home';
 // else
 // 	$home = 'home_login';
-=======
->>>>>>> 095504fe6e4c163743d35209be449e3173145b2a
 
 if (isset($_SESSION['id'],$_SESSION['login']))
 {
@@ -27,7 +21,6 @@ else
 {
 	require('views/home_login.phtml');
 }
->>>>>>> 90368ab10000a737e202c1f5b515f21c89fec3d0
 
 // require('views/home.phtml');
 // require('views/home_login.phtml');
